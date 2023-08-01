30d1dec5af82e76ee6ba31a7e105d8ba348ead1a
Brian Soares <brian@snapandstyle.com>
2023-07-31 23:43:38 -0400

    UI changes.

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -3,7 +3,7 @@
     <nav>
         <div class="nav nav-tabs justify-content-end" id="nav-tab" role="tablist">
             <button class="nav-link active" id="nav-ymys-game-log-tab" data-bs-toggle="tab" data-bs-target="#nav-ymys-game-log" type="button" role="tab" aria-controls="nav-ymys-game-log" aria-selected="true">Ymys Game Log</button>
-            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+            <!-- <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button> -->
             <button class="nav-link" id="nav-drafthistory-tab" data-bs-toggle="tab" data-bs-target="#nav-drafthistory" type="button" role="tab" aria-controls="nav-drafthistory" aria-selected="false">Draft History</button>
         </div>
     </nav>
@@ -12,13 +12,13 @@
         <div class="tab-pane fade show active" id="nav-ymys-game-log" role="tabpanel" aria-labelledby="nav-ymys-game-log-tab">
             <player-stats :playerStats="this.playerStats" :player="this.player" />
         </div>
-        <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+        <!-- <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
             <div class="row">
                 <div class="col-12">
                     <team-matchups />
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="tab-pane fade" id="nav-drafthistory" role="tabpanel" aria-labelledby="nav-drafthistory-tab">
             <player-draft-history v-if="this.draftHistory" :draftHistory="this.draftHistory" />
         </div>
